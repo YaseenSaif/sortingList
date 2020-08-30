f63b32a921fed5a2b6aa78831f9b452122abd148
Yaseen Saif AlShaibani <yasenshibane77@gmail.com>
2020-08-31 00:09:17 +0300

    init commit

---
+++ src/main.kt
@@ -0,0 +1,7 @@
+fun main() {
+var list = listOf(8,4,7,1,2,3,0,5,6)
+    println("list: $list")
+    println("ascending order of list: ${list.sorted()}");
+    println("descending order of list: ${list.sortedDescending()}");
+
+}
